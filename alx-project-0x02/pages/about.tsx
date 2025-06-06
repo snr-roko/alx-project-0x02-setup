86d36aa5825a1f44bb5be1954b4a2e06c1dad661
snr-roko <rbbagyei@outlook.com>
2025-06-06 17:51:20 +0000

    feat: add About component with title and team section

---
+++ alx-project-0x02/pages/about.tsx
@@ -0,0 +1,12 @@
+const About : React.FC = () => {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold text-center text-gray-500 mt-3">The Genius Game</h1>
+      <div className="flex flex-col md:flex-row md:space-x-4">
+        <h2 className="text-2xl font-semibold text-gray-500 mt-3 text-center">Team Behind</h2>
+      </div>
+    </div>
+  )
+}
+
+export default About;
