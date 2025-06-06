3cb2a7e73dca417ac4c861909807958019a7999a
snr-roko <rbbagyei@outlook.com>
2025-06-06 12:56:48 +0000

    feat: initialize alx-project-2 with Next.js, TypeScript, and Tailwind CSS
    - Added package.json with dependencies for React, Next.js, TypeScript, and Tailwind CSS.
    - Created _app.tsx to set up global layout with a Header component.
    - Implemented _document.tsx for custom document structure.
    - Added a sample API route in hello.ts.
    - Created index.tsx as the main landing page with a welcome message.
    - Configured PostCSS for Tailwind CSS.
    - Added SVG assets for icons and images.
    - Included global CSS styles for theming and layout.
    - Set up TypeScript configuration for the project.

---
+++ alx-project-2/pages/_app.tsx
@@ -0,0 +1,13 @@
+import Header from "@/components/layout/Header";
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+  <div className="min-h-screen">
+    <Header />
+    <Component {...pageProps} />
+  </div>
+  )
+  
+}
---
+++ alx-project-2/pages/index.tsx
@@ -0,0 +1,10 @@
+const Home: React.FC = () => {
+  return (
+    <main className="p-5 flex flex-col justify-center items-center">
+      <h2>Welcome To the Genius Game</h2>
+    </main>
+  )
+}
+
+
+export default Home;
